11180b05435f7c90c128851fdd45a8824e2c70ce
dg <1961767103@qq.com>
2025-02-13 12:37:45 +0800

    补充静态图片

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,12 +1,13 @@
 <template>
 	<view class="app">
-		<view class="title"><!-- 征婚公告 --></view>
+		<view class="title">征婚公告</view>
 		<view class="selecttitle">请选择您的姓名：</view>
-		<uni-section class="select" type="line">
+		<uni-section class="select" type="line" >
 		    <uni-data-select class="option"
 				v-model="namevalue"
 		        :localdata="name"
 				:clear="false"
+				@change="nameChange"
 		    ></uni-data-select>
 		</uni-section>
 		<hr/>
@@ -21,27 +22,34 @@
 		<hr/>
 		<view class="selecttitle">请输入您的资产：</view>
 		<view class="select2">
-			&InvisibleComma;
-			&InvisibleComma;
-			&InvisibleComma;
+			<view class="konge"></view>
 			<input class="rich" v-model="rich" type="number"/>
 			<view class="wanyuan">
 				万元
 			</view>
 		</view>
 		<button @click="handleClick" style="background-color: rgba(255, 255, 255, 0.3);">开始匹配</button>
+		
+		<view class="box" >
+			<image v-if="namevalue=='彤彤咪'" class="pic" src="../../static/tt.jpg" mode="aspectFill"></image>
+			<image v-if="namevalue=='静静咪'" class="pic" src="../../static/yj.jpg" mode="aspectFill"></image>
+			<image v-if="namevalue=='宁宁咪'" class="pic" src="../../static/zn.jpg" mode="aspectFill"></image>
+			<image v-if="showStatue==1" class="pic" src="../../static/ym.jpg" mode="aspectFill"></image>
+		</view>
 	</view>
+	
 </template>
 
 <script setup>
 	import { ref } from 'vue';
 	let namevalue = ref();
 	let value = ref();
-	let showStatue = ref();
+	let showStatue = ref(0);
 	let rich = ref(0);
 	let name = ref([
-	      { value: "陈紫啧", text: "陈紫啧" },
-	      { value: "坐下姐", text: "坐下姐" }
+	      { value: "彤彤咪", text: "彤彤咪" },
+	      { value: "静静咪", text: "静静咪" },
+	      { value: "宁宁咪", text: "宁宁咪" }
 	]);
 	let range = ref([
 	      { value: 0, text: "答辩型" },
@@ -49,7 +57,7 @@
 	      { value: 2, text: "暴躁型" },
 	]);
 	function handleClick(){
-		console.log(rich.value)
+		showStatue.value=false;
 		if(namevalue.value==null){
 			alert("请先选择姓名！");
 			return;
@@ -58,7 +66,7 @@
 			alert("请先选择类型！");
 			return;
 		}
-		if(rich.value<50){
+		if(rich.value<10){
 			alert("谈什么谈，赚钱去");
 			return;
 		}
@@ -66,8 +74,13 @@
 			alert("我就说"+namevalue.value+"是富婆吧")
 			return;
 		}
-		if(rich.value>=50){
+		if(rich.value>=100){
 			alert("我不信，除非你v我500");
+			return;
+		}
+		else{
+			showStatue.value=1;
+			alert("匹配成功");
 			return;
 		}
 	}
@@ -77,7 +90,9 @@
 	.app{
 		position: absolute;
 		height: 100vh;
-		background-color: pink;
+		background:
+		linear-gradient(to bottom,rgba(0,0,0,0),#fff ),
+		linear-gradient(to right,#beecd8 20%,#f4e2d8);
 		.title{
 			font-size: 50rpx;
 			display: flex;
@@ -91,20 +106,39 @@
 		.select2{
 			display: inline-block;
 			width: 60vw;
-			background-color: rgba(255, 255, 255, 0.5);
+			background: rgba(255, 255, 255, 0.8);
+		}
+		.konge{
+			display: inline-block;
+			width: 5vw;
 		}
 		.rich{
 			display: inline-block;
+			width: 45vw;
 		}
 		.wanyuan{
 			display: inline-block;
+			width: 10vw;
 			
 		}
 		.select{
 			display: inline-block;
 			width: 60vw;
 			.option{
-				background: rgba(255, 255, 255, 0.5);
+				background: rgba(255, 255, 255, 0.8);
+			}
+		}
+		.box{
+			width: 300rpx;
+			height: 330rpx;
+			margin-right: 15rpx;
+			position: absolute;
+			top: 30%;
+			left: 30%;
+			.pic{
+				height: 100%;
+				width: 100%;
+				border-radius: 10rpx;
 			}
 		}
 	}
